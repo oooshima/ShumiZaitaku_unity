ef931ab61294b05e731381a59a8d131865b1d25f
Yuki Y <kydmy128@gmail.com>
2018-04-21 18:33:10 +0900

    ひらがのスクリプト足したよ

---
+++ Assets/Hiraga/measureAcceleration.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class  measureAcceleration : MonoBehaviour
+{
+	int counter = 1;
+	int sum = 0;
+	bool isEating = true;
+
+	[SerializeField] Text score;
+	[SerializeField] Text score2;
+	[SerializeField] Text score3;
+
+	// Use this for initialization
+	void Start ()
+	{
+		Input.gyro.enabled = true;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+		counter++;
+		// 加速度センサの値を取得
+		double valx = Input.acceleration.x;
+		double valy = Input.acceleration.y;
+		double valz = Input.acceleration.z;
+		Vector3 val = Input.acceleration;
+
+
+		if (-1.0 < valy && valy < -0.8) {
+			sum += 1;
+		}
+
+		//score.text = val.ToString(); //sum.ToString(); //val.ToString();
+		score2.text = valy.ToString ();
+		score.text = sum.ToString ();
+	}
+
+	public void OnClick ()
+	{
+		Finish ();
+	}
+
+	public int Finish ()
+	{
+		
+		isEating = false;
+		var result = 100 * sum / counter;
+		score3.text = "SCOREは" + result.ToString ();
+		return result;
+	}
+}
---
+++ Assets/Yamada/MeasureSceneController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MeasureSceneController : MonoBehaviour {
+
+	[SerializeField] private GameObject resultPanel;
+	[SerializeField] private GameObject resultCarrots;
+	[SerializeField] private GameObject measureSystem;
+	[SerializeField] private Text pointText;
+
+	// Use this for initialization
+	void Start () {
+		resultPanel.SetActive(false);		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	int Score(int rate) {
+		return rate / 20 + 1;
+	}
+
+	public void OnClickStopButton() {
+		resultPanel.SetActive(true);
+		int score = Score(measureSystem.GetComponent<Measure>().Finish());
+		foreach (Transform t in resultCarrots.transform) {
+			if (Int32.Parse(t.name.Substring(6)) <= score) t.gameObject.SetActive(true);
+			else t.gameObject.SetActive(false);
+		}
+		pointText.text = "にんじんを " + score.ToString() + " ほん かくとくした！";
+	}
+
+	public void OnClickReturnButton() {
+		// SceneManager.LoadScene("Oshima");
+	}
+}
